b921b70bcf0b974ae848400c97a07f1eaebc254c
visheshguria <vishesh.guriaa94@gmail.com>
2022-11-09 15:24:39 +0530

    Packages Created

---
+++ industrious-summer-462/src/test/java/com/masai/IndustriousSummer462ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustriousSummer462ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
